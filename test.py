b0a6076cc5aa5113777a6fac90625c79c0cf26f4
Lily Li <maxandfish@gmail.com>
2020-09-07 11:48:05 -0400

    updates

---
+++ test.py
@@ -0,0 +1,12 @@
+import numpy as np
+import seaborn as sns
+sns.set()
+import matplotlib.pyplot as plt
+import pandas as pd
+
+x = np.array([1,2,3,4,5])
+y = np.array([1,2,3,4,5])
+df = pd.DataFrame(x,y)
+
+sns.relplot(data = df)
+plt.show()
